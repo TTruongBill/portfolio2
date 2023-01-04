fd7f51b7d1bff404ff8ba7bab41d7a62ee0fe960
Tien Tien Truong <tientientruong@live.fr>
2023-01-03 19:20:22 -0500

    Finished the css of the footer **need to finish the project section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,70 @@
 import './sass/Footer.scss'
+import { useState } from 'react';
+import {sendEmail} from '../emailJS';
+import EmailIcon from '@mui/icons-material/Email';
+import PhoneIcon from '@mui/icons-material/Phone';
+import HomeIcon from '@mui/icons-material/Home';
 
 export default function Footer(){
+    const [toSend, setToSend] = useState({
+        from_name: '',
+        from_nameFam: '',
+        to_name: '',
+        message: '',
+        number: '',
+        reply_to: '',
+      });
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        sendEmail(toSend);
+    };
+
+    const handleChange = (e) => {
+        setToSend({ ...toSend, [e.target.name]: e.target.value });
+      };
+
     return(
         <div className="foot" id="foot">
-            <h1>Contact</h1>
-            <form action="" className='contactForm'>
-                <input id="name" type="text" placeholder='Nom'/>
-                <input id="email" type="email" placeholder='Email'/>
-                <textarea name="content" id="content" placeholder='Message'></textarea>
-                <input id="submit" type="submit" />
-            </form>
+            <h2>Contact</h2>
+            <div id="footerInfo">
+                <div id="contact">
+                    <h1>Envoyez Un Courriel</h1>
+                    <p></p>
+                    <form onSubmit={onSubmit} className='contactForm'>
+                        <p>Prénom</p>
+                        <input id="name" type="text" name='from_name' value={toSend.from_name} onChange={handleChange}/>
+                        <p>Nom*</p>
+                        <input required id="nameFam" type="text" name='from_nameFam' value={toSend.from_nameFam} onChange={handleChange}/>
+                        <p>Courriel*</p>
+                        <input required id="email" placeholder="ex: monnom@example.com"type="email" name='reply_to' value={toSend.reply_to} onChange={handleChange}/>
+                        <p>Téléphone*</p>
+                        <input required id="number" placeholder="ex: 514-999-9999" type="tel" name='number' value={toSend.number} onChange={handleChange} pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"/>
+                        <p>Message*</p>
+                        <textarea required id="content" name='message' value={toSend.message} onChange={handleChange}></textarea>
+                        <input id="submit" type="submit" />
+                    </form>
+                </div>
+                <div id='myInfo'>
+                <h1>Mes Informations</h1>
+                <p></p>
+                    <div id="infoContent">
+                        <div className='info'>
+                            <EmailIcon/>
+                            <p>tientientruong1@gmail.com</p>
+                        </div>
+                        <div className='info'>
+                            <PhoneIcon/>
+                            <p>514-830-5213</p>
+                        </div> 
+                        <div className='info'>
+                            <HomeIcon/>
+                            <p>Montréal,QC</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <p id="copyright">&copy;Tien Tien Bill Truong, 2023</p>
         </div>
     )
 }
