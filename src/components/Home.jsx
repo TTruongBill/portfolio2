956bd901b0dadb4580c4130f517fceb3c74269e4
Tien Tien Truong <tientientruong@live.fr>
2023-01-13 14:54:51 -0500

    Customized icon/Favicon/homepage picture

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@ import './sass/Home.scss'
 export default function Home(){
     return(
         <section id='home'>
-            <img src="./images/guyIcon.svg" alt="guyIcon" />
+            <img src="./images/tienIcon.svg" alt="guyIcon" />
             <div className='contenuHome'>
                 <h1>Bonjour, je suis</h1>
                 <h2>Tien Tien Truong</h2>
